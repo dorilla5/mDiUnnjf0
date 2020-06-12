494c2e26d4a54205bceb8ff6c1f9d17fc0fea3f8
Pántya Tamás <tamas.pantya@loxon.eu>
2020-06-12 23:11:36 +0200

    Requesting status in Queue microservice.

---
+++ reboarding/microservices/queue/src/main/java/com/fu/mDiUnnjf0/queue/entity/Queue.java
@@ -0,0 +1,29 @@
+package com.fu.mDiUnnjf0.queue.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Queue {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue( strategy= GenerationType.IDENTITY)
+    private Integer id;
+    private String userName;
+    private Integer indexOfRegistration;
+
+    public Queue( final String userName, final Integer indexOfRegistration ) {
+        this.userName = userName;
+        this.indexOfRegistration = indexOfRegistration;
+    }
+}
