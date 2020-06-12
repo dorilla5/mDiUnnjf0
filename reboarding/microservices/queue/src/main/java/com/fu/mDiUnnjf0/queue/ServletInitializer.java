c2fb000256902fbfec81ed4de4815fde9d128caf
Böhm Dorottya <dorilla5@gmail.com>
2020-06-12 21:31:21 +0200

    Rethink microservices

---
+++ reboarding/microservices/queue/src/main/java/com/fu/mDiUnnjf0/queue/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.fu.mDiUnnjf0.queue;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(QueueApplication.class);
+	}
+
+}
